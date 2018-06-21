61f2f76e915189cc09b86b69e842e1d2f63ef9de
Bharath Phatak <Bphatak@exenet.com>
2018-06-21 14:53:03 +0530

    I

---
+++ src/main/java/MyThreads.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyThreads {
+    private Object object = new Object();
+    private static final int COUNT = 20;
+    public static void main(String args[]) throws InterruptedException{
+        MyThreads t = new MyThreads();
+
+        System.out.println("Check Mado HARRIIIIISSSSSSHHHHHHHH");
+        //I dont think you need to pass this atomic object in your case.
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        for(int i=0;i< COUNT;i++){
+           Thread tt =new Thread(t.new MyThread(i,atomicInteger));
+           tt.setName("Thread " + i);
+           tt.start();
+
+       }
+
+    }
+
+
+    private class MyThread implements Runnable{
+        private final int threadCount;
+        AtomicInteger atomicInteger;
+        public MyThread(int threadCount,AtomicInteger value) {
+            super();
+            this.threadCount = threadCount;
+            this.atomicInteger = value;
+        }
+
+
+        public void run() {
+            while (this.atomicInteger.get() < COUNT) {
+
+                synchronized (object) {
+
+                    if (this.atomicInteger.get()  == this.threadCount) {
+
+                        if(this.atomicInteger.get() < COUNT)
+                            System.out.println(Thread.currentThread()+"  "+
+                                    this.atomicInteger.incrementAndGet());
+                    }
+                }
+            }
+        }
+    }
+}
